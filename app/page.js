d5991a956d3e7f838e4c67db9b490a30214ab5c6
Rishabh Pathak <90998740+Coup-DeFoudre@users.noreply.github.com>
2024-04-12 12:52:10 +0530

    v2

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import {
   FaTwitter,
 } from "react-icons/fa6";
 import Link from "next/link";
-import Footer from "../Components/footer"
+
 
 const Home = () => {
   return (
@@ -147,7 +147,26 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <Footer />
+        <div>
+        <div className="socials-mobile lg:hidden bottom-0 left-0 right-0 p-5 flex justify-center items-center">
+      <h4 className="mr-3">Follow us at :</h4>
+      <div className="flex gap-3">
+        <Link
+          href="https://www.facebook.com/share/ABUsMtg4erWFAP2y/?mibextid=qi2Omg"
+          target="_blank"
+        >
+          {" "}
+          <FaFacebook />{" "}
+        </Link>
+        <Link href="https://www.instagram.com/thepaltan.in/" target="_blank">
+          {" "}
+          <FaInstagram />
+        </Link>
+
+        <FaTwitter />
+      </div>
+    </div>
+        </div>
       </div>
     </div>
   );
